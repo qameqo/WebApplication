6637fc7ea71a29769a428411458814ef63141d28
saintnaja12 <teetouchnaja@gmail.com>
2019-12-02 13:29:58 +0700

    DB CONFIG

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -18,6 +18,12 @@ class test extends CI_Controller {
         
     }
 
+    public function DB_Check()
+    {
+        $this->db->where('id', 555);
+        $this->db->get('tests'); 
+    }
+
 }
 
 /* End of file test.php */
