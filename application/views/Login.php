5bbc01499f27052354e42b3f69dbe6946a133972
atipat rongmuang <gameza-12@hotmail.com>
2019-12-03 15:56:49 +0700

    dff

---
+++ application/views/Login.php
@@ -0,0 +1,46 @@
+<!-- <div class="container">
+  <div class="row">
+    <div class="col-sm">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <div class="row">
+    <div class="col-sm">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <button type="button" class="btn btn-success">Login</button>
+    </div>
+    </div>
+  </div>
+</div> -->
+<div class="photo"></div>
+<div class="container">
+	<div class="row">
+		<div class="col">
+		</div>
+		<div class="col">
+			<label for="exampleInputEmail1">Email address</label>
+			<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+				placeholder="Enter email">
+		</div>
+		<div class="col">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+		</div>
+		<div class="col">
+			<label for="exampleInputPassword1">Password</label>
+			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		</div>
+		<div class="col">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+		</div>
+		<div class="col">
+            <button type="button" class="btn btn-success">Login</button>
+		</div>
+		<div class="col">
+		</div>
+	</div>
