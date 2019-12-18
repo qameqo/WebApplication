99d391529a6f5e75b8f4b95e7fe80dd996c5008d
atipat rongmuang <gameza-12@hotmail.com>
2019-12-18 12:17:00 +0700

    dfdf

--- application/views/Login.php
+++ application/views/Login.php
@@ -16,7 +16,7 @@
 <!-- Login -->
 
 
-<div class="container">
+<!-- <div class="container">
 	<div class="photo mt-5 mb-3" style="text-align: center;">
 		<img src="<?php echo base_url('./pic/logo.png'); ?>" />
 	</div>
@@ -35,4 +35,22 @@
 			</form>
 		</div>
 	</div>
-</div>
+</div> -->
+<div class="photo mt-5 mb-3" style="text-align: center;">
+    <img src="<?php echo base_url('./pic/logo.png'); ?>" />
+    </div>
+	<div class="row justify-content-center">
+		<div class="col-sm-4 shadow p-3 mb-5 bg-white rounded"
+			style="background-color: #5DBCD2; border-radius: 10px ; margin-left: 1em ; margin-right: 1em ;">
+<form>
+  <div class="form-group">
+    <h4>Email address</h4>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email...">
+  </div>
+  <div class="form-group">
+    <h4>Password</h4>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password...">
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+  <button type="submit" class="btn btn-primary">Register</button>
+</form>
--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
-<head>
+<!-- <head>
 
 	<meta charset="utf-8" />
-	<!-- <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png"> -->
+	 <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png"> 
 	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>./pic/logo.jpg">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<title>
@@ -9,15 +9,26 @@
 	</title>
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
 		name='viewport' />
-	<!--     Fonts and icons     -->
+	     Fonts and icons     -->
 	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-	<!-- CSS Files -->
+	<!-- CSS Files 
 
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/.min.css?v=2.0.6">
 
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/paper-dashboard.css?v=2.0.0">
-	
-</head>
+</head> -->
 
+<head>
+    <title>G Dragon Autotech CO.,LTD</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>./pic/logo.jpg">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!--     Fonts and icons     -->
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <!-- Material Kit CSS -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/material-kit.css?v=2.0.6">
+</head>
 <body>
