06806996d8c681b484530bc7f366795175646376
saintnaja12 <teetouchnaja@gmail.com>
2019-12-28 15:15:45 +0700

    5

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,15 +1,20 @@
-
-     <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
-    <!-- Core -->
-  <script src="<?php echo base_url(); ?>assets argon/vendor/jquery/jquery.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets argon/vendor/popper/popper.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets argon/vendor/bootstrap/bootstrap.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets argon/vendor/headroom/headroom.min.js"></script>
-  <!-- Argon JS -->
-  <script src="<?php echo base_url(); ?>assets argon/js/argon.js?v=1.1.0"></script>
-  <!--Datepicker -->
-  <script src="<?php echo base_url('/assets argon/vendor/bootstrap-datepicker/js/bootstrap-datepicker.min.js'); ?>"></script>
-  <script type="text/javascript">
+<!--   Core JS Files   -->
+<script src="<?php echo base_url(); ?>assets/js/core/jquery.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/core/popper.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/plugins/moment.min.js"></script>
+    <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+    <script src="<?php echo base_url(); ?>assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+    <script src="<?php echo base_url(); ?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+    <!--  Google Maps Plugin  -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+    <script src="<?php echo base_url(); ?>assets/js/material-kit.js?v=2.0.6" type="text/javascript"></script>
+</body>
+<script type="text/javascript">
 
       function Change_Brand()
       {
@@ -23,13 +28,13 @@
               }
           );
       }
- </script>      
- <!--year picker-->
-<script type="text/javascript">
+
+      // Year Picker 
       $('.date-own').datepicker({
          minViewMode: 2,
          format: 'yyyy'
        });
+
   </script>
-</body>
+
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -1,13 +1,67 @@
 <head>
+
     <title>G Dragon Autotech CO.,LTD</title>
     <link rel="icon" type="image/png" href="<?php echo base_url(); ?>./pic/logo.jpg">
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-  <!-- Icons -->
-  <link href="<?php echo base_url(); ?>assets argon/vendor/nucleo/css/nucleo.css" rel="stylesheet">
-  <link href="<?php echo base_url(); ?>assets argon/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <!-- Argon CSS -->
-  <link type="text/css" href="<?php echo base_url(); ?>/assets argon/css/argon.css?v=1.1.0" rel="stylesheet">
-  
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <!-- AJAX -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Data Table -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+    <!-- Material Kit CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/material-kit.css?v=2.0.6" rel="stylesheet" />
+
+    <nav class="navbar  fixed-top navbar-expand-lg bg-danger">
+        <div class="container">
+            <div class="navbar-translate">
+                <a class="navbar-brand" href="/presentation.html">G Dragon Autotech</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#pablo" class="nav-link" style="font-size: 16px;">จัดการข้อมูลพนักงาน</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#pablo" class="nav-link">link</a>
+                    </li>
+                </ul>
+
+                <form class="form-inline ml-auto mr-2">
+                    <div class="form-group no-border">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-white btn-just-icon btn-round">
+                        <i class="material-icons">search</i>
+                    </button>
+                </form>
+                <button type="button" class="btn btn-primary">
+                    Notifications <span class="badge badge-default">4</span>
+                </button>
+            </div>
+        </div>
+    </nav>
+
 </head>
-<body>
+
+<body style="font-family: 'Quark', 'Quark', sans-serif;">
