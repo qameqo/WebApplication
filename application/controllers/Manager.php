76055f7d024a0929946a0ea652935dac4c57dace
saintnaja12 <teetouchnaja@gmail.com>
2020-01-03 13:49:49 +0700

    2

--- application/controllers/Manager.php
+++ application/controllers/Manager.php
@@ -6,14 +6,14 @@ class Manager extends CI_Controller {
     public function Manager_1()
     {   
         $this->load->view('Header_manage');
-        $this->load->view('Manager_emp');
+        $this->load->view('Manager');
         $this->load->view('Footer_manage');
     } 
 
-    public function test2()
+    public function Manager_emp()
     {
         $this->load->view('Header_manage');
-        // $this->load->view('Manager_emp');
+        $this->load->view('Manager_emp');
         $this->load->view('Footer');
     }
 
