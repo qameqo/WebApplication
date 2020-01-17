d186d156e67b5d3541721b27cac7a9902ff2784f
saintnaja12 <teetouchnaja@gmail.com>
2020-01-17 12:41:49 +0700

    2

--- application/views/dataregis.php
+++ application/views/dataregis.php
@@ -1,9 +1,9 @@
 <form action="<?php echo base_url('Dataregis/show'); ?>">
-            <div class="col-md-12 text-center"><br>
-                <h4 class="" style="color:#000000">ข้อมูลการลงทะเบียนรถยนต์</h4>
-                <div class="row justify-content-center">
-					<div class="col-sm-10 shadow p-4 mb-4 bg-white rounded">
-                    <?php 
+    <div class="col-md-12 text-center"><br>
+        <h4 class="" style="color:#000000">ข้อมูลการลงทะเบียนรถยนต์</h4>
+        <div class="row justify-content-center">
+            <div class="col-sm-10 shadow p-4 mb-4 bg-white rounded">
+                <?php 
                         
                         $this->db->select('*');
                         $this->db->from('Carregis');
@@ -14,38 +14,39 @@
                         $qq = $query->result_array();
                         
 						?>
-						<div class="col-md-12 mb-5 text-center">
-                        <br>
-                <table id="employee_data" class="table table-striped table-bordered text-center" style="width: 1200px;">
+                <div class="col-md-12 mb-5 text-center">
+                    <br>
+                    <table id="employee_data" class="table table-striped table-bordered text-center"
+                        style="width: 1200px;">
                         <thead>
                             <tr>
-								<th data-column-id="Name_image" style="width: 150px;">ยี่ห้อ</th>
-                                <th data-column-id="Name_image"style="width: 200px;">รุ่น</th>
-                                <th data-column-id="Name_image"style="width: 200px;">ทะเบียน</th>
-                                <th data-column-id="Name_image"style="width: 350px;">ปีที่ออกรถ</th>
-                                <th data-column-id="Name_image"style="width: 300px;">ที่นั่ง</th>
-                                <th data-column-id="Name_image"style="width: 100px;">สี</th>
-                                <th data-column-id="Name_image"style="width: 320px;">ชนิดเชื้อเพลิง</th>
-                                <th data-column-id="Name_image"style="width: 200px;">เกียร์</th>
-                                <th data-column-id="Name_image"style="width: 350px;">ปีที่ออกทะเบียน</th>
-                                <th data-column-id="Name_image"style="width: 250px;">เลขตัวถัง</th>
-                                <th data-column-id="Name_image"style="width: 150px;">สถานะ</th>
-                                <th data-column-id="Name_image"style="width: 150px;"></th>
+                                <th data-column-id="Name_image" style="width: 150px;">ยี่ห้อ</th>
+                                <th data-column-id="Name_image" style="width: 200px;">รุ่น</th>
+                                <th data-column-id="Name_image" style="width: 200px;">ทะเบียน</th>
+                                <th data-column-id="Name_image" style="width: 350px;">ปีที่ออกรถ</th>
+                                <th data-column-id="Name_image" style="width: 300px;">ที่นั่ง</th>
+                                <th data-column-id="Name_image" style="width: 100px;">สี</th>
+                                <th data-column-id="Name_image" style="width: 320px;">ชนิดเชื้อเพลิง</th>
+                                <th data-column-id="Name_image" style="width: 200px;">เกียร์</th>
+                                <th data-column-id="Name_image" style="width: 350px;">ปีที่ออกทะเบียน</th>
+                                <th data-column-id="Name_image" style="width: 250px;">เลขตัวถัง</th>
+                                <th data-column-id="Name_image" style="width: 150px;">สถานะ</th>
+                                <th data-column-id="Name_image" style="width: 150px;"></th>
                             </tr>
                         </thead>
-						<tbody>
-                        <?php foreach($qq as $data){
+                        <tbody>
+                            <?php foreach($qq as $data){
 								
                                 ?>
-							<tr>
-								<td><?php echo $data['Name_Brand'];?></td>
-								<td><?php echo $data['Name_Gen'];?></td>
+                            <tr>
+                                <td><?php echo $data['Name_Brand'];?></td>
+                                <td><?php echo $data['Name_Gen'];?></td>
                                 <td><?php echo $data['License'];?></td>
-								<td><?php echo $data['Yearcar'];?></td>
+                                <td><?php echo $data['Yearcar'];?></td>
                                 <td><?php echo $data['Seat'];?></td>
-								<td><?php echo $data['Color'];?></td>
+                                <td><?php echo $data['Color'];?></td>
                                 <td><?php echo $data['Fuel'];?></td>
-								<td><?php echo $data['Gear'];?></td>
+                                <td><?php echo $data['Gear'];?></td>
                                 <td><?php echo $data['Yearlicense'];?></td>
                                 <td><?php echo $data['Carbody'];?></td>
                                 <!-- <td><span class="badge badge-warning"><?php //echo $data['Status'];?></span></td> -->
@@ -87,16 +88,16 @@
                                     echo '</span></td>';
                                 } ?>
 
-								<td>
+                                <td>
                                     <a href="<?php echo site_url('Dataregis2') ?>" class="btn btn-info btn-sm">View</a>
                                 </td>
 
-								</tr>
-                                <?php 
+                            </tr>
+                            <?php 
 							} ?>
-						</tbody>
+                        </tbody>
                     </table>
-                    </div>
-                    </div>
-                    </div>
-                    </form>
+                </div>
+            </div>
+        </div>
+</form>
