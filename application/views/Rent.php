ff35863f2d345a039f264cda3a8211a393d1ce1f
atipat rongmuang <gameza-12@hotmail.com>
2020-01-18 18:14:55 +0700

    fff

---
+++ application/views/Rent.php
@@ -0,0 +1,52 @@
+<div class="col-md-13 text-center"><br>
+	<h1 class="h1" style="color:#000000">เช่ารถยนต์</h1>
+</div>
+<div class="row justify-content-center">
+	<div class="col-sm-10 shadow p-3 mb-5 bg-white rounded"
+		style="background-color: #FFFFFF; border-radius: 10px ; margin-left: 1em ; margin-right: 1em ;">
+<form class="form-signin" id="edit" name="edit" method="post"action="">
+
+            
+			<div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+            <div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+			<div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+			<div class="row">
+			<div class="col-sm">
+				
+			</div>
+		    </div>
+            <div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+            <div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+            <div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+            <div class="row">
+			<div class="col-sm">
+			
+			</div>
+			</div>
+</div>
+</div>
+</form>
