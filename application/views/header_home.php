9c4a60a5b37e0abf63dc4f160e06835a00c93a56
atipat rongmuang <gameza-12@hotmail.com>
2020-01-20 23:57:08 +0700

    jjj

--- application/views/header_home.php
+++ application/views/header_home.php
@@ -23,21 +23,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbar-default">
-            <div class="navbar-collapse-header">
-                <div class="row">
-                    <div class="col-6 collapse-brand">
-                        <a href="index.html">
-                            <img src="assets/img/brand/blue.png">
-                        </a>
-                    </div>
-                    <div class="col-6 collapse-close">
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-default" aria-controls="navbar-default" aria-expanded="false" aria-label="Toggle navigation">
-                            <span></span>
-                            <span></span>
-                        </button>
-                    </div>
-                </div>
-            </div>
+            
 
             <ul class="navbar-nav ml-lg-auto">
                 <li class="nav-item">
