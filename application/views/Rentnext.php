f1a7a9c0d9c0e810c3bfadd2fa656c5e6d8c3bd6
atipat rongmuang <gameza-12@hotmail.com>
2020-01-20 17:36:08 +0700

    fddgdg

--- application/views/Rentnext.php
+++ application/views/Rentnext.php
@@ -1,4 +1,4 @@
-<?php echo "ไอสัส";
+<?php 
       $this->db->select('*');
      $this->db->from('Carregis');
      $this->db->join('Brand', 'Carregis.Brand = Brand.idBrand');
@@ -7,8 +7,31 @@
      $this->db->where('idCarregis',$idc);
      $query = $this->db->get();
      $qq = $query->result_array();
-?>
-    
-<?php foreach($qq as $data){?> 
-    <?php echo $data['RentalPrice']; ?>
-<?php } ?>
+    ?>
+    <div class="col-md-13 text-center"><br>
+    <?php foreach($qq as $data){ ?>
+	<h4 style="color:#000000"><?php echo $data['Name_Brand'];?>&nbsp;<?php echo $data['Name_Gen'];?>&nbsp;<?php echo $data['Yearcar'];?></h4>
+    <?php } ?>
+</div>
+<form class="form-signin" id="edit" name="edit" method="post" action="">
+	<div class="row justify-content-center">
+      
+		<div class="col-sm-5 shadow p-3 mb-5 bg-white rounded"
+			style="background-color: #FFFFFF; border-radius: 10px ; margin-left: 1em ; margin-right: 1em ;">
+            <H4 style="text-align:center;">รูปภาพเกี่ยวกับรถคันนี้</H4>
+            <br>
+            <?php 
+      $this->db->select('*');
+      $this->db->from('Images');
+      $this->db->where('idCarregis',$idc);
+      $query = $this->db->get();
+      $q2 = $query->result_array();
+    ?>
+            <?php foreach($q2 as $data){ ?>
+			<img src="<?php echo base_url('./pic/'.$data['Name_image']);?>" style="height:80px; weight:80px;">
+            <?php } ?>
+		</div>
+		<div class="col-sm-3 shadow p-3 mb-5 bg-white rounded"
+			style="background-color: #FFFFFF; border-radius: 10px ; margin-left: 1em ; margin-right: 1em ;">
+	</div>
+</form>
