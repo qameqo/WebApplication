df3b4f54bcb585968c27f69aeca3d1c191605347
saintnaja12 <teetouchnaja@gmail.com>
2020-03-06 00:56:01 +0700

    s

--- application/views/Manager/Manager_car_rental_view_2.php
+++ application/views/Manager/Manager_car_rental_view_2.php
@@ -4,10 +4,35 @@
 		<div class="panel panel-default">
             <div class="panel-heading">
                 <div class="row">
-                    <div class="col-md-10">
+                    <div class="col-md-9">
                         <h3 class="panel-title">ข้อมูลรายได้ทั้งหมด</h3>
                         
                     </div>
+                    <!-- <div class="col-md-3">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" style="font-size: 16px;">
+                        รายได้รวมทั้งหมดของทางร้าน
+                        </button>
+
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
                 </div>
 
             </div>
