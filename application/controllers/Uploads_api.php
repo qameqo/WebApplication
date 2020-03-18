6503ecee126e23dbe9a0e3b098081ac565f1015a
atipat rongmuang <gameza-12@hotmail.com>
2020-03-18 17:43:50 +0700

    aaa

--- application/controllers/Uploads_api.php
+++ application/controllers/Uploads_api.php
@@ -18,46 +18,58 @@ require APPPATH . '/libraries/REST_Controller.php';
  */
 class Uploads_api extends \Restserver\Libraries\REST_Controller {
     
-    public function index_post($idp){
-    $config['upload_path'] = './img4/';
-    $config['allowed_types'] = 'gif|jpg|png';
-    $config['max_size']  = '1000000000';
-    $config['max_width']  = '1000000000';
-    $config['max_height']  = '1000000000';
+ 
+        public function index_post(){
     
-    $this->load->library('upload', $config);
-    $this->upload->initialize($config);
-   
-    
-          
-    if ($this->upload->do_upload('file')){
-        $this->response(array(
+        $config['upload_path'] = './img4/';
+        $config['allowed_types'] = '*';
+        $config['max_size']  = '1000000000';
+        $config['max_width']  = '1000000000';
+        $config['max_height']  = '1000000000';
+        
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+        if ( ! $this->upload->do_upload('file')){
+            $error = array('error' => $this->upload->display_errors());
+            $this->response(array(
+                'status' => 'false',
+                 'error' => $error
+                ));
+        }
+        else{
+            $data = array('upload_data' => $this->upload->data());
+             $this->response(array(
             'status' => 'true',
              'error' => 'kk'
-        ));
-             $data = $this->upload->data();
-
-            $filename = $data['file_name'];
-            //$imgtype_name = $data['imgtype_name'];
-            $arr=array(
-                                'Name_image4'=>$filename,
-                                'id_Problem'=>$idp
-            );
-                            
-            $this->db->insert('Images4', $arr);
-
-    }
-    else{
-      
-    $error = array('error' => $this->upload->display_errors());
-        $this->response(array(
-            'status' => 'false',
-            'error' => $error
-            // 'error' => $error
-
-        ));
-    }
-}
+            ));
+            // $filename = $data['file_name'];
+            // $arr=array(
+            //     'Name_image4'=>$filename,
+            //     'id_Problem'=>$idp
+            // );
+            
+            // $this->db->insert('Images4', $arr);
+            
+        
+        }
+        // $config['upload_path'] = './img4/';
+        // $config['allowed_types'] = '*';
+        // $config['max_size']  = '1000000000';
+        // $config['max_width']  = '1000000000';
+        // $config['max_height']  = '1000000000';
+        
+        // $this->upload->initialize($config);
+        
+        // if ( ! $this->upload->do_upload('file')){
+        //     $error = array('error' => $this->upload->display_errors());
+        // }
+        // else{
+        //     $data = array('upload_data' => $this->upload->data());
+            
+        // }
+    
+    
+        }
     
 
 }
